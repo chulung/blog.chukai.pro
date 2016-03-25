1e9ce5e811de0865c9cd65ea2e3350d92db7727d
chukai <chukai@chukai.pro>
2016-03-25 20:32:43 +0800

    移除外部js lib

--- smelting/src/test/java/smelting/DataUtil.java
+++ smelting/src/test/java/smelting/DataUtil.java
@@ -1,5 +1,9 @@
 package smelting;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:com/yihaodian/badge/config/spring/spring.xml" })
 public class DataUtil {
