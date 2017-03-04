f2d0f002ba47c5f12cdaa2426a385c66ac235b5b
chukai <chukai@chukai.pro>
2017-03-04 23:33:31 +0800

    remove unuse import

--- website/src/test/java/com/chulung/test/BaseTest.java
+++ website/src/test/java/com/chulung/test/BaseTest.java
@@ -3,7 +3,6 @@ package com.chulung.test;
 import com.chulung.Application;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
