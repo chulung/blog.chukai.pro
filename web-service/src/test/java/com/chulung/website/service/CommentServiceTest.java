bdf5a6c0f5ee601e1aad3dc423d2c5721191b64c
chukai <chukai@chukai.pro>
2017-04-11 11:47:34 +0800

    feat: blog blog-cms

---
+++ web-service/src/test/java/com/chulung/website/service/CommentServiceTest.java
@@ -0,0 +1,38 @@
+package com.chulung.website.service;
+
+import com.chulung.BaseTest;
+import com.chulung.website.exception.HttpStatusException;
+import com.chulung.website.model.Comment;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by chulung on 2017/4/6.
+ */
+public class CommentServiceTest extends BaseTest {
+    @Autowired
+    private CommentService commentService;
+
+    @Test(expected = HttpStatusException.class)
+    public void postComments() throws Exception {
+        Comment comment = new Comment();
+        comment.setArticleId(1);
+        comment.setEmail("abc@qq.com");
+        comment.setUserName("name");
+        assertThat(commentService.postComments(comment)).isTrue();
+        commentService.postComments(comment);
+    }
+
+    @Test
+    public void listComments() throws Exception {
+        assertThat(commentService.listComments(1,1).getList()).isNotEmpty();
+    }
+
+    @Test
+    public void findRecentlyComments() throws Exception {
+        assertThat(commentService.findRecentlyComments()).isNotEmpty();
+    }
+
+}
