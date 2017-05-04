56df0b7619f225e4e4ee02c2334d19c014c2c1c9
chukai <chukai@chukai.pro>
2017-05-04 22:31:42 +0800

    fix: test config

--- web-service/src/test/java/com/chulung/BaseTest.java
+++ web-service/src/test/java/com/chulung/BaseTest.java
@@ -1,15 +1,11 @@
 package com.chulung;
 
-import com.chulung.Application;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.EnvironmentAware;
-import org.springframework.core.env.Environment;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 @PowerMockIgnore( {"javax.management.*"})
 public abstract class BaseTest{
     static {
+        //使用测试环境配置 src/test/resources/application-test.yml
         System.setProperty("spring.profiles.active","test");
     }
 }
