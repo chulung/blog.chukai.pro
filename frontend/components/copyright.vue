20798a5adc64b993b5f9ae94a9390b8302e9a5da
chukai <chukai@chukai.pro>
2018-01-21 15:58:02 +0800

    docs:update copyright

--- frontend/components/copyright.vue
+++ frontend/components/copyright.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-6 copy-left">
-          Copyright © 2015-2017 <a href="/"><strong>初开</strong></a>. All Right Reserved.
+          Copyright © 2015-2017 <a href="/"><strong>初开</strong></a>. All Right Reserved.<a href="http://www.miitbeian.gov.cn">湘ICP备15017845号</a>
         </div><!-- .col-sm-6 -->
         <div class="col-sm-6 copy-right">
           <ul class="social-icon">
