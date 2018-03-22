9af2fcf2c7e7453d453e0b54b6956abe64b6b088
Chukai <wenchukai@qq.com>
2018-03-22 13:12:52 +0800

    Update navigation.vue

--- blog/components/navigation.vue
+++ blog/components/navigation.vue
@@ -9,7 +9,7 @@
               <h1 class="site-title title-image"><a href="index.html" rel="home"><img src="assets/img/danish-image-logo.png" alt="Danish."></a></h1>
               -->
               <h1 class="site-title">
-                <router-link to="/" rel="home">初开之道.</router-link>
+                <router-link to="/" rel="home">初开之道.(本博客已不再维护，请查看我的作品库：http://chukai.pro)</router-link>
               </h1>
               <p class="site-description">记录我的人生之道。//</p>
             </div><!-- .site-branding -->
